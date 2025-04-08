7fe566452b15c03358a91a6cc5bb6a9c96d441a9
Jessica Kline <kline.jessica@gmail.com>
2025-04-08 10:29:52 -0500

    contact start

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,11 @@
 import { Routes } from '@angular/router';
 import { DanceComponent } from './dance/dance.component';
+import { ContactComponent } from './contact/contact.component';
 import { ScheduleComponent } from './schedule/schedule.component';
 
 export const routes: Routes = [
     { path: "dance", component: DanceComponent },
-    { path: "schedule", component: ScheduleComponent }
+    { path: "schedule", component: ScheduleComponent },
+    { path: "dance", component: DanceComponent },
+    { path: "contact", component: ContactComponent}
 ];
