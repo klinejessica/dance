e4500ce5b6a90a737915d27cb294beb03087bbbb
Jessica Kline <kline.jessica@gmail.com>
2025-04-08 10:29:52 -0500

    contact start

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
-import { RouterLinkActive, RouterOutlet } from '@angular/router';
+import { RouterOutlet } from '@angular/router';
 
 import { HeaderComponent } from './header/header.component';
 
 @Component({
     selector: 'app-root',
-    imports: [RouterOutlet, RouterLinkActive,  HeaderComponent],
+    imports: [RouterOutlet, HeaderComponent],
     templateUrl: './app.component.html',
     styleUrl: './app.component.scss'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { DanceComponent } from './dance/dance.component';
+import { ContactComponent } from './contact/contact.component';
 
 export const routes: Routes = [
-    { path: "dance", component: DanceComponent }
+    { path: "dance", component: DanceComponent },
+    { path: "contact", component: ContactComponent}
 ];
