f2a9e7cce03af8f94013809c28778452b3cd42ba
Jessica Kline <kline.jessica@gmail.com>
2025-05-01 15:47:59 -0500

    added google map

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -4,13 +4,20 @@ import { Component } from '@angular/core';
 import {MatSelectModule} from '@angular/material/select';
 import {MatInputModule} from '@angular/material/input';
 import {MatFormFieldModule} from '@angular/material/form-field';
+import { GoogleMapsModule } from '@angular/google-maps';
+import { NgForOf } from '@angular/common';
 
 @Component({
   selector: 'app-contact',
-  imports: [MatSelectModule, MatFormFieldModule, MatInputModule],
+  imports: [MatSelectModule, MatFormFieldModule, MatInputModule, GoogleMapsModule,NgForOf],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.scss'
 })
 export class ContactComponent {
+
+  center: google.maps.LatLngLiteral = { lat: 6.2440317440866, lng: -75.57948896663142};
+  markerLatLong: google.maps.LatLngLiteral[] = [{ lat: 6.243644005067747, lng: -75.5793795041487}];
+
+
 
 }
