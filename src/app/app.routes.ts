623b88f3f7d4783b2f378770eb916fbb19174692
Jessica Kline <kline.jessica@gmail.com>
2025-05-08 14:09:02 -0500

    Add FAQ

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { DanceComponent } from './dance/dance.component';
 import { ContactComponent } from './contact/contact.component';
 import { ScheduleComponent } from './schedule/schedule.component';
+import { FaqsComponent } from './faq/faq.component';
 
 export const routes: Routes = [
     { path: "dance", component: DanceComponent },
     { path: "schedule", component: ScheduleComponent },
     { path: "dance", component: DanceComponent },
-    { path: "contact", component: ContactComponent}
+    { path: "contact", component: ContactComponent},
+    { path: "faq", component: FaqsComponent}
 ];
