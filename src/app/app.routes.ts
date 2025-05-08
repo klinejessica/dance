2bef8ea1bcc30a4a9602684984ece0cb5a30a42e
Jessica Kline <kline.jessica@gmail.com>
2025-05-08 14:22:28 -0500

    created instructors component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,11 +3,13 @@ import { DanceComponent } from './dance/dance.component';
 import { ContactComponent } from './contact/contact.component';
 import { ScheduleComponent } from './schedule/schedule.component';
 import { FaqsComponent } from './faq/faq.component';
+import { InstructorsComponent } from './instructors/instructors.component';
 
 export const routes: Routes = [
     { path: "dance", component: DanceComponent },
     { path: "schedule", component: ScheduleComponent },
     { path: "dance", component: DanceComponent },
     { path: "contact", component: ContactComponent},
-    { path: "faq", component: FaqsComponent}
+    { path: "faq", component: FaqsComponent},
+    { path: "instructors", component: InstructorsComponent}
 ];
