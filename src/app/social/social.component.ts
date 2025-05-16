1f60d6cbfadde868d8f56e6a874387b307aa4d34
Jessica Kline <kline.jessica@gmail.com>
2025-05-16 11:05:37 -0500

    social page

--- src/app/social/social.component.ts
+++ src/app/social/social.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatGridListModule } from '@angular/material/grid-list';
 
 @Component({
   selector: 'app-social',
-  imports: [],
+  imports: [MatButtonModule, MatGridListModule, MatCardModule],
   templateUrl: './social.component.html',
   styleUrl: './social.component.scss'
 })
